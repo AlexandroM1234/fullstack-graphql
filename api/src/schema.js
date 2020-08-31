c75b314e4e8f1cdef3d81e231fe0522553a2556b
Alex Martinez <alexmartinez0071234@gmail.com>
2020-08-30 22:44:45 -0400

    debugged code

--- api/src/schema.js
+++ api/src/schema.js
@@ -1,4 +1,4 @@
-const { gql } = require("graphql-tag");
+const { gql } = require("apollo-server");
 const { ApolloServer } = require("apollo-server");
 
 /**
@@ -13,9 +13,10 @@ const typeDefs = gql`
 
   type Query {
     me: User!
+    friends: [User]!
   }
 `;
-const resolver = {
+const resolvers = {
   Query: {
     me() {
       return {

e4b6feef2a93c74571b392f1307ce423cec59438
Alex Martinez <alexmartinez0071234@gmail.com>
2020-08-30 22:21:29 -0400

    following the video

--- api/src/schema.js
+++ api/src/schema.js
@@ -1,11 +1,38 @@
-const { gql } = require('apollo-server')
+const { gql } = require("graphql-tag");
+const { ApolloServer } = require("apollo-server");
 
 /**
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+  type User {
+    email: String!
+    avatar: String
+    friends: [User]!
+  }
 
+  type Query {
+    me: User!
+  }
 `;
+const resolver = {
+  Query: {
+    me() {
+      return {
+        email: "yoda@masters.com",
+        avatar: "http://yoda.png",
+        friends: [],
+      };
+    },
+  },
+};
 
-module.exports = typeDefs
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen(4000).then(() => {
+  console.log("YOU MADE A GRAPHQL SERVER");
+});
+module.exports = typeDefs;

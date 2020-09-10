8765c28c047e2748c362fa5c1e8a977994c6b1ee
Alex Martinez <alexmartinez0071234@gmail.com>
2020-09-10 12:28:48 -0400

    added a missing bracket

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -5,7 +5,6 @@ import PetsList from "../components/PetsList";
 import NewPetModal from "../components/NewPetModal";
 import Loader from "../components/Loader";
 
-
 const ALL_PETS = gql`
   query AllPets {
     pets {
@@ -39,34 +38,37 @@ export default function Pets() {
 
   if (error || newPet.error) {
     return <p>{error}</p>;
-    
-  const onSubmit = (input) => {
-    setModal(false);
-    createPet({
-      variables: { newPet: input },
-    });
-  };
 
-  if (modal) {
-    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />;
+    const onSubmit = (input) => {
+      setModal(false);
+      createPet({
+        variables: { newPet: input },
+      });
+    };
+
+    if (modal) {
+      return (
+        <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
+      );
+    }
+
+    return (
+      <div className="page pets-page">
+        <section>
+          <div className="row betwee-xs middle-xs">
+            <div className="col-xs-10">
+              <h1>Pets</h1>
+            </div>
+
+            <div className="col-xs-2">
+              <button onClick={() => setModal(true)}>new pet</button>
+            </div>
+          </div>
+        </section>
+        <section>
+          <PetsList pets={data.pets} />
+        </section>
+      </div>
+    );
   }
-
-  return (
-    <div className="page pets-page">
-      <section>
-        <div className="row betwee-xs middle-xs">
-          <div className="col-xs-10">
-            <h1>Pets</h1>
-          </div>
-
-          <div className="col-xs-2">
-            <button onClick={() => setModal(true)}>new pet</button>
-          </div>
-        </div>
-      </section>
-      <section>
-        <PetsList pets={data.pets} />
-      </section>
-    </div>
-  );
 }

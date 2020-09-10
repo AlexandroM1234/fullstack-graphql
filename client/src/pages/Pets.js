98f6a0bc0cf7ad0ff323192841467ce90663669b
Alex Martinez <alexmartinez0071234@gmail.com>
2020-09-10 12:29:33 -0400

    fixed again

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -38,37 +38,35 @@ export default function Pets() {
 
   if (error || newPet.error) {
     return <p>{error}</p>;
-
-    const onSubmit = (input) => {
-      setModal(false);
-      createPet({
-        variables: { newPet: input },
-      });
-    };
-
-    if (modal) {
-      return (
-        <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
-      );
-    }
-
-    return (
-      <div className="page pets-page">
-        <section>
-          <div className="row betwee-xs middle-xs">
-            <div className="col-xs-10">
-              <h1>Pets</h1>
-            </div>
-
-            <div className="col-xs-2">
-              <button onClick={() => setModal(true)}>new pet</button>
-            </div>
-          </div>
-        </section>
-        <section>
-          <PetsList pets={data.pets} />
-        </section>
-      </div>
-    );
   }
+
+  const onSubmit = (input) => {
+    setModal(false);
+    createPet({
+      variables: { newPet: input },
+    });
+  };
+
+  if (modal) {
+    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />;
+  }
+
+  return (
+    <div className="page pets-page">
+      <section>
+        <div className="row betwee-xs middle-xs">
+          <div className="col-xs-10">
+            <h1>Pets</h1>
+          </div>
+
+          <div className="col-xs-2">
+            <button onClick={() => setModal(true)}>new pet</button>
+          </div>
+        </div>
+      </section>
+      <section>
+        <PetsList pets={data.pets} />
+      </section>
+    </div>
+  );
 }
